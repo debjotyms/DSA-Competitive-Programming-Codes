26b13cb8d971bde14c725d81c5b485861d407ae0
debjotyms <debjotyms@gmail.com>
2022-04-16 00:57:14 +0600

    More Codes.

---
+++ HackerEarth/Terrible_Chandu.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t; cin>>t;
+    while (t--)
+    {
+        string a; cin>>a;
+        reverse(a.begin(),a.end());
+        cout<<a<<endl;
+    }
+}
---
+++ Homework/p.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define LL long long
+#define PB push_back
+#define MP make_pair
+#define VI vector <int>
+#define VIN for(int &x:v) cin>>x;
+#define FL(A,B,C,D) for (int A=B;A<C;A+=D)
+using namespace std;
+void solve(){
+    int a = 100;
+    int *pa = &a;
+    int **ppa = &pa;
+    cout<<pa<<endl<<ppa;
+}
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
