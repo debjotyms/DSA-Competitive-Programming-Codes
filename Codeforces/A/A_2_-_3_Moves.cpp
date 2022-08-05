7af79e61060b5b2e740ed68dadf59fa834b19164
Debjoty Mitra <debjotyms@gmail.com>
2022-08-05 10:59:24 +0600

    2-3 Moves

---
+++ Codeforces/A/A_2_-_3_Moves.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector <long long>
+#define vin for(auto &x:v) cin>>x;
+#define vout for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+void solve(){
+    ll n; cin>>n;
+    if(n==1){
+        cout<<2<<endl;
+        return;
+    }
+    cout<<n/3+(n%3?1:0)<<endl;
+}
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
