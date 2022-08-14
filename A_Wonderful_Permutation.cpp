31d9d62c0b3bcc796ce800ced17cd1ebcd815ce2
Debjoty Mitra <debjotyms@gmail.com>
2022-08-14 11:08:22 +0600

    A. Wonderful Permutation

---
+++ A_Wonderful_Permutation.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector <long long>
+#define vin for(auto &x:v) cin>>x;
+#define vout for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+void solve(){
+    int n,k; cin>>n>>k;
+    vi v(n); vin;
+    int ans = 0;
+    for(int i=0;i<k;i++){
+        if(v[i]>k){
+            ans++;
+        }
+    }
+    cout<<ans<<endl;
+}
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
