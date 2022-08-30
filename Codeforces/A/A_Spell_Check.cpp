b3b1ef5f323371ab852bdc9f474483a021564f3b
Debjoty Mitra <debjotyms@gmail.com>
2022-08-30 23:35:37 +0600

    A. Spell Check

---
+++ Codeforces/A/A_Spell_Check.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector <long long>
+#define vin for(auto &x:v) cin>>x;
+#define vout for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+void solve(){
+    int n; cin>>n;
+    string s; cin>>s;
+    sort(s.begin(),s.end());
+    if(s.size()==5){
+        if(s[0]=='T'&&s[1]=='i'&&s[2]=='m'&&s[3]=='r'&&s[4]=='u') cout<<"YES"<<endl;
+        else cout<<"NO\n";
+    }else{
+        cout<<"NO\n";
+    }
+}
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
