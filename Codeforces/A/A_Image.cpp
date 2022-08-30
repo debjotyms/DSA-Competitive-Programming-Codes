994d44a5257a57111eaf112694b9077dafc9610c
Debjoty Mitra <debjotyms@gmail.com>
2022-08-30 23:34:03 +0600

    A. Image

---
+++ Codeforces/A/A_Image.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector <long long>
+#define vin for(auto &x:v) cin>>x;
+#define vout for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+void solve(){
+    string s1; cin>>s1;
+    string s2; cin>>s2;
+    set<char> st;
+    st.insert(s1[0]);
+    st.insert(s1[1]);
+    st.insert(s2[0]);
+    st.insert(s2[1]);
+    int type = st.size();
+    cout<<type-1<<endl;
+}
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
