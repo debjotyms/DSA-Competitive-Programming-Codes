afe63ab1cccc963988eaf0c2e9181ad970000f8e
Debjoty Mitra <debjotyms@gmail.com>
2022-09-01 10:24:13 +0600

    C. Unique Number

---
+++ Codeforces/C/C_Unique_Number.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector <long long>
+#define vin for(auto &x:v) cin>>x;
+#define vout for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+void solve(){
+    int x; cin>>x;
+    int sum = 0;
+    vector<int> st;
+    for(int i=9;i>0;i--){
+        if(sum+i>x){
+            continue;
+        }else{
+            st.push_back(i);
+            sum+=i;
+        }
+    }
+    if(sum==x){
+        sort(st.begin(),st.end());
+        for(auto i:st) cout<<i;
+        cout<<endl;
+    }else{
+        cout<<-1<<endl;
+    }
+}
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
