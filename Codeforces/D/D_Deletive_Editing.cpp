cbf110eca83fc159f6ad226c254e18c9baa53f1d
Debjoty Mitra <debjotyms@gmail.com>
2023-02-04 16:51:26 +0600

    Deletive Editing

---
+++ Codeforces/D/D_Deletive_Editing.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector <long long>
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+string charRemover(string s, char ch){
+    auto it = find(all(s),ch);
+    if(it!=s.end()) s.erase(it);
+    return s;
+}
+void solve(){
+    string sa,sb; cin>>sa>>sb;
+    map<char,int> mp;
+    for(int i=0;i<sa.size();i++) mp[sa[i]]++;
+    for(int i=0;i<sb.size();i++){
+        mp[sb[i]]--;
+        if(mp[sb[i]]<0){
+            cout<<"NO\n";
+            return;
+        }
+    }
+    string s;
+    for(int i=0;i<sa.size();i++){
+        if(mp[sa[i]]==0) s.push_back(sa[i]);
+        else mp[sa[i]]--; 
+    }
+    if(sb==s) cout<<"YES\n";
+    else cout<<"NO\n";
+}
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
