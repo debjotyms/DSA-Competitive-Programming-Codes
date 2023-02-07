ba3cb9754449dc0f5c820070206bb3851b1bbafc
Debjoty Mitra <debjotyms@gmail.com>
2023-02-07 19:57:58 +0600

    Democracy in Danger

---
+++ Timus/1025_Democracy_in_Danger.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector <long long>
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+void solve(){
+    ll n; cin>>n;
+    vi v(n); vin;
+    sort(all(v));
+    ll ans=0;
+    for(int i=0;i<n/2+1;i++){
+        ans+=v[i]/2+1;
+    }
+    cout<<ans<<endl;
+}
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    //cin>>t;
+    while(t--) solve();
+    return 0;
+}
