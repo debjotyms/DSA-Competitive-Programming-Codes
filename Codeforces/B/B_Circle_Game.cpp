b63caeaefe553a7ce7a31ec036a4f1515aa79484
Debjoty Mitra <debjotyms@gmail.com>
2023-02-07 19:56:58 +0600

    Circle Game

---
+++ Codeforces/B/B_Circle_Game.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector <long long>
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+void solve(){
+    int n; cin>>n;
+    vi v(n); vin;
+    if(n%2==1){
+        cout<<"Mike\n";
+    }else{
+        int minIndx = 0;
+        for(int i=0;i<n;i++){
+            if(v[i]<v[minIndx]){
+                minIndx = i;
+            }
+        }
+        if(minIndx%2==0) cout<<"Joe\n";
+        else cout<<"Mike\n";
+    }
+}
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
