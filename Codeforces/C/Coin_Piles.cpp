b315cc39eec1e060cd99de01921a0e42565bcedf
Debjoty Mitra <debjotyms@gmail.com>
2023-02-10 22:57:20 +0600

    Coin Piles

---
+++ Codeforces/C/Coin_Piles.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector <long long>
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+void solve(){
+    ll a,b; cin>>a>>b;
+    if(a*2==b or b*2==a) cout<<"Yes\n";
+    else if(abs(a-b)==2){
+        
+    }
+}
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
