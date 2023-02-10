88132d18ab5f332a02b4683c533de70e95b797ef
Debjoty Mitra <debjotyms@gmail.com>
2023-02-10 22:55:39 +0600

    C++ Array

---
+++ Playground/Array.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a[10]={1};
+    for(int i=0;i<10;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<10;i++){
+        cout<<a[i]<<" ";
+    }
+}
