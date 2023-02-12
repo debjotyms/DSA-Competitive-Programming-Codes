99d002c6f575def3cf38e46ce75d9a6d23d11c1a
Debjoty Mitra <debjotyms@gmail.com>
2023-02-12 22:50:03 +0600

    DIV MOD

---
+++ Codeforces/B/B_DIV_MOD.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector <long long>
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+void solve(){
+    int l,r,a; cin>>l>>r>>a;
+    int x=r-(r%a)-1>=l?r-(r%a)-1:x=r;
+    ll ans = (x/a)+(x%a);
+    ll ans2 = (r/a)+(r%a);
+    cout<<max(ans,ans2)<<endl;
+}
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
