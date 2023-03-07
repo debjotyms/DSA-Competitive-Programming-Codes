f1c3c895be814896c68a9eeb2054c2ec4fbc5e1e
Debjoty Mitra <debjotyms@gmail.com>
2023-03-07 13:08:38 +0600

    If at first you don't succeed

---
+++ Codeforces/A/A_If_at_first_you_don_t_succeed.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector <long long>
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+void solve(){
+    int a,b,c,d; cin>>a>>b>>c>>d;
+    int rem = d-(a+b-c);
+    if(rem<=0) cout<<-1<<endl;
+    else if(a<c || b<c) cout<<-1<<endl;
+    else cout<<rem<<endl;
+}
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    //cin>>t;
+    while(t--) solve();
+    return 0;
+}
