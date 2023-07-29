81f926be8683ab321c763042ed57eb872d0cf0c9
Debjoty Mitra <debjotyms@gmail.com>
2023-07-29 20:25:14 +0600

    B. Bright Nice Brilliant

---
+++ Codeforces/B/B_Bright_Nice_Brilliant.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector <long long>
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+void solve(){
+    int n; cin>>n;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<=i;j++){
+			if(j==0 || j==i) cout<<1<<" ";
+			else cout<<0<<" ";
+		}
+		cout<<endl;
+	}
+}
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
