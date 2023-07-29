d2b015e7754d84c4ade0063f3ee9a8fdd794c48a
Debjoty Mitra <debjotyms@gmail.com>
2023-07-29 13:39:03 +0600

    B. Roof Construction

---
+++ Codeforces/B/B_Roof_Construction.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector <long long>
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+void solve(){
+    int n; cin>>n; n--;
+    while((n&(n-1))!=0){
+        cout<<n<<" ";
+        n--;
+    }
+    cout<<n<<" ";
+    for(int i=0;i<n;i++) cout<<i<<" ";
+    cout<<endl;
+}
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
