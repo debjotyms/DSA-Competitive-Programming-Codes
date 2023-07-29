cf204e627ed443da596d00ac7d2ddd65d2dcfc48
Debjoty Mitra <debjotyms@gmail.com>
2023-07-29 13:32:56 +0600

    A. Divisible Array

---
+++ Codeforces/A/A_Divisible_Array.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector <long long>
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+void solve(){
+    int n; cin>>n;
+    if(n%2==0){
+        int j=2;
+        for(int i=1;i<=n;i++){
+            cout<<j<<" ";
+            j+=2;
+        }
+        cout<<endl;
+    }else{
+        for(int i=1;i<=n;i++) cout<<i<<" ";
+        cout<<endl;
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
