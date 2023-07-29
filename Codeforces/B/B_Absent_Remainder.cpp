dbf089dc70dd4dcb0a12e5f06aa2fa47d750271c
Debjoty Mitra <debjotyms@gmail.com>
2023-07-29 20:22:17 +0600

    B. Absent Remainder

---
+++ Codeforces/B/B_Absent_Remainder.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector <long long>
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+void solve(){
+    int n; cin>>n;
+    vi v(n); vin;
+    sort(all(v));
+    reverse(all(v));
+    for(int i=0;i<(n/2);i++) cout<<v[i]<<" "<<v[n-1]<<endl;
+}
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
