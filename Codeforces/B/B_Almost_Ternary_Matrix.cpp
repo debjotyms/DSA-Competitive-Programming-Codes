5ed4595e25ceedd5e53d905baf63183ddab0f7b5
Debjoty Mitra <debjotyms@gmail.com>
2023-07-29 20:22:32 +0600

    B. Almost Ternary Matrix

---
+++ Codeforces/B/B_Almost_Ternary_Matrix.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector <long long>
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+void solve(){
+    int n,m; cin>>n>>m;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            cout<<((i%4<=1)==(j%4<=1))<<" ";
+        }
+        cout<<endl;
+    }
+}
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
