507a549070187908f097564f9e752ac48d82e5a5
Debjoty Mitra <debjotyms@gmail.com>
2023-07-30 20:28:41 +0600

    A. Dalton the Teacher

---
+++ Codeforces/A/A_Dalton_the_Teacher.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector <long long>
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+void solve(){
+    ll n; cin>>n;
+    vi v(n); vin;
+    ll res = 0;
+    for(ll i=1;i<=n;i++){
+        if(v[i-1]==i) res++;
+    }
+
+    if(res%2==0){
+        cout<<(res/2)<<endl;
+    }else{
+        cout<<(res/2)+1<<endl;
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
