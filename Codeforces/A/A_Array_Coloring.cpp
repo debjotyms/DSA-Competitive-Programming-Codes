9469f33c6aa2e3cb2857a9179bf87c5dedfa5bf0
Debjoty Mitra <debjotyms@gmail.com>
2023-08-09 11:49:38 +0600

    A. Array Coloring

---
+++ Codeforces/A/A_Array_Coloring.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector <long long>
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+void solve(){
+    ll n; cin>>n;
+    ll od=0, ev=0;
+
+    while(n--){
+        ll a; cin>>a;
+        if(a%2==0) ev++;
+        else od++;
+    }
+
+    if(od%2==0) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
