7275000b09abc3c52f8ee1ef9849fc8a1801df6e
Debjoty Mitra <debjotyms@gmail.com>
2023-08-09 11:49:27 +0600

    A. Subtraction Game

--- Codeforces/A/A_Subtraction_Game.cpp
+++ Codeforces/A/A_Subtraction_Game.cpp
@@ -1,28 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define vi vector <long long>
+#define all(x) x.begin(),x.end()
 #define vin for(auto &x:v) cin>>x;
 #define vout for(auto x:v) cout<<x<<" ";
 #define endl '\n'
 typedef long long ll;
 long long cs = 1;
+
 void solve(){
-    int n; cin>>n;
-    bool chk = true;
-    ll sum = 0;
-    for(int i=0;i<n-1;i++){
-        ll a; cin>>a;
-        if(chk && a==0){
-            continue;
-        }else{
-            chk = false;
-        }
-        if(a==0) sum++;
-        else sum+=a; 
-    }
-    cout<<sum<<endl;
+    ll a,b; cin>>a>>b;
+    cout<<a+b<<endl;
 }
-int32_t main(){
+
+int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
