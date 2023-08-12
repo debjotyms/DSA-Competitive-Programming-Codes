80a07a2795866250ef729698f4f3bb14be96425d
Debjoty Mitra <debjotyms@gmail.com>
2023-08-12 20:25:45 +0600

    A. The Man who became a God

---
+++ Codeforces/A/A_The_Man_who_became_a_God.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector <long long>
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+void solve(){
+    ll n,k; cin>>n>>k;
+    vi v(n); vin;
+
+    ll sum = 0;
+    vi dif;
+
+    for(ll i=0;i<n-1;i++){
+        dif.push_back(abs(v[i]-v[i+1]));
+    }
+
+    sort(all(dif));
+
+    for(ll i=0;i<n-k;i++) sum+=dif[i];
+
+    cout<<sum<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
