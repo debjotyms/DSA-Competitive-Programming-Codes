ecdb6b63b2ddafebece509cc9f5584148654fab3
Debjoty Mitra <debjotyms@gmail.com>
2023-08-29 09:05:08 +0600

    A. Plus or Minus

---
+++ Codeforces/A/A_Plus_or_Minus.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector <long long>
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+void solve(){
+    ll a,b,c; cin>>a>>b>>c;
+    if(a+b==c) cout<<"+"<<endl;
+    else cout<<"-"<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
