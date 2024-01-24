f86643ef6b1764fce5ca6eceb2772f286c0b96cf
Debjoty Mitra <debjotyms@gmail.com>
2024-01-24 21:08:03 +0600

    1327. Fuses

---
+++ Timus/1327_Fuses.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define vi vector <long long>
+#define rev(x) reverse(all(x))
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout(v) for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+void solve(){
+    ll a, b; cin>>a>>b;
+    if(a%2==0) a+1;
+    if(b%2==0) b-=1;
+    if(a<=b) cout<<((b-a)/2)+1<<endl;
+    else cout<<0<<endl;
+
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    //cin>>t;
+    while(t--) solve();
+    return 0;
+}
