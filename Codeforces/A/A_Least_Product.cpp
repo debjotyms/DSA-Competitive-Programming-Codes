d6e55fc81925caabc0e02f999c85bfbe2c43ddf3
Debjoty Mitra <debjotyms@gmail.com>
2024-01-24 20:49:54 +0600

    A. Least Product

---
+++ Codeforces/A/A_Least_Product.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define vi vector <long long>
+#define rev(x) reverse(all(x))
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout(v) for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+void solve(){
+    ll n; cin>>n;
+    ll count = 0;
+    ll zero = 0;
+    for(ll i=0;i<n;i++){
+        ll a; cin>>a;
+        if(a==0) zero++;
+        if(a<0) count++; 
+    }
+    if(zero) cout<<0<<endl;
+    else if(count%2==0){
+        cout<<1<<endl;
+        cout<<1<<" "<<0<<endl;
+    }else{
+        cout<<0<<endl;
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
