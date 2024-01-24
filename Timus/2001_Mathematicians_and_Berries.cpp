2fe08e73f6ebe8f11c7d7de885497340f0f5aec2
Debjoty Mitra <debjotyms@gmail.com>
2024-01-24 21:13:35 +0600

    2001. Mathematicians and Berries

---
+++ Timus/2001_Mathematicians_and_Berries.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define vi vector <long long>
+#define rev(x) reverse(all(x))
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout(v) for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+void solve(){
+    ll a1, b1, a2, b2, a3, b3; cin>>a1>>b1>>a2>>b2>>a3>>b3;
+    cout<<a1-a3<<" "<<b1-b2;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    //cin>>t;
+    while(t--) solve();
+    return 0;
+}
