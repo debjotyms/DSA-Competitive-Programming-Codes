be7717e2d2f1b69350bf56c302e369a978731501
Debjoty Mitra <debjotyms@gmail.com>
2024-01-24 21:14:57 +0600

    2100. Wedding Dinner

---
+++ Timus/2100_Wedding_Dinner.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define vi vector <long long>
+#define rev(x) reverse(all(x))
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout(v) for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+void solve(){
+    ll n; cin>>n;
+    ll count = 2;
+    while(n--){
+        string s; cin>>s;
+        if(s.find("+one") != string::npos) count+=2;
+        else count+=1;
+    }
+    if(count==13) count++;
+    cout<<count*100<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    //cin>>t;
+    while(t--) solve();
+    return 0;
+}
