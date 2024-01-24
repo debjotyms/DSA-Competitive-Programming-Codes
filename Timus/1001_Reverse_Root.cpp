c56f98c8fd250cea029a611d016e33fbdeb0163d
Debjoty Mitra <debjotyms@gmail.com>
2024-01-24 20:59:12 +0600

    1001. Reverse Root

--- Timus/1001_Reverse_Root.cpp
+++ Timus/1001_Reverse_Root.cpp
@@ -22,3 +22,37 @@ int32_t main(){
     solve();
     return 0;
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define pb push_back
+// #define vi vector <long long>
+// #define rev(x) reverse(all(x))
+// #define all(x) x.begin(),x.end()
+// #define vin for(auto &x:v) cin>>x;
+// #define vout(v) for(auto x:v) cout<<x<<" ";
+// #define endl '\n'
+// typedef long long ll;
+// long long cs = 1;
+
+// void solve(){
+//     double t;
+//     stack<double> st;
+//     while(cin>>t){
+//         st.push(sqrt(t));
+//     }
+
+//     while(!st.empty()){
+//         cout<<fixed<<setprecision(4)<<st.top()<<endl;
+//         st.pop();
+//     }
+// }
+
+// int main(){
+//     ios_base::sync_with_stdio(0);
+//     cin.tie(0);
+//     cout.tie(0);
+//     int t=1;
+//     while(t--) solve();
+//     return 0;
+// }
