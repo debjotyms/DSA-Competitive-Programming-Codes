8128ce51e97f7c8bafbeee23396815024413a20a
Debjoty Mitra <debjotyms@gmail.com>
2024-01-24 20:54:23 +0600

    B. Chip and Ribbon

---
+++ Codeforces/B/B_Chip_and_Ribbon.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define vi vector <long long>
+#define rev(x) reverse(all(x))
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout(v) for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+void solve(){
+    ll n; cin>>n;
+    vi v(n); vin;
+
+    v.push_back(0);
+
+    ll ans = 0;
+
+    for(ll i=0;i<n;i++){
+        if(v[i]>v[i+1]) ans+=(v[i]-v[i+1]);
+    }
+
+    cout<<ans-1<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
