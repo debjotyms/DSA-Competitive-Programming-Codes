d6d2d189368fb9d53125bbb2dd993117f51a6eeb
Debjoty Mitra <debjotyms@gmail.com>
2024-01-24 21:12:06 +0600

    1924. Four Imps

---
+++ Timus/1924_Four_Imps.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define vi vector <long long>
+#define rev(x) reverse(all(x))
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout(v) for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+void solve(){
+    ll n; cin>>n;
+    n = (n*(n+1))/2;
+    if(n%2==0) cout<<"black"<<endl;
+    else cout<<"grimy"<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    //cin>>t;
+    while(t--) solve();
+    return 0;
+}
