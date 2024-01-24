53866bcd4e57d169132db5fe12df1192b16c7183
Debjoty Mitra <debjotyms@gmail.com>
2024-01-24 20:58:31 +0600

    D. Gold Rush

---
+++ Codeforces/D/D_Gold_Rush.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define vi vector <long long>
+#define rev(x) reverse(all(x))
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout(v) for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+bool gold(ll n, ll b){
+    if(n==b) return true;
+    if(n%3!=0) return false;
+    return gold(n/3LL, b) or gold((n/3LL)*2LL, b);
+}
+
+void solve(){
+    ll a, b; cin>>a>>b;
+    bool res = gold(a,b);
+    if(res) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
