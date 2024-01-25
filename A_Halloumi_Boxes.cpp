d7c4f6668e21998fb0414fadc6df28dd4159c2e9
Debjoty Mitra <debjotyms@gmail.com>
2024-01-25 20:06:57 +0600

    A. Halloumi Boxes

---
+++ A_Halloumi_Boxes.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define vi vector <long long>
+#define rev(x) reverse(all(x))
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout(v) for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+void solve(){
+    ll n,k; cin>>n>>k;
+    vi v(n); vin;
+    vi vv = v;
+    sort(all(vv));
+    if(k==1){
+        if(v==vv) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }else{
+        cout<<"YES"<<endl;
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
