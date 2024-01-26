40812f10d5a53bad460dccb1fcba788f8a18a17d
Debjoty Mitra <debjotyms@gmail.com>
2024-01-27 00:44:59 +0600

    C. Vasilije in Cacak

---
+++ Codeforces/C/C_Vasilije_in_Cacak.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define vi vector <long long>
+#define rev(x) reverse(all(x))
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout(v) for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+void solve(){
+    ll n,k,x; cin>>n>>k>>x;
+    ll lower=((k*(k+1))/2);
+    ll upper=((n*(n+1))/2)-(((n-k)*(n-k+1))/2);
+    if(x>=lower and x<=upper) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
