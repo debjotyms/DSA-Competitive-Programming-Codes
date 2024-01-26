3501c5873e0f3f9493262a2642cf5841377e629c
Debjoty Mitra <debjotyms@gmail.com>
2024-01-26 23:59:13 +0600

    1225. Flags

---
+++ Timus/1225_Flags.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define vi vector <long long>
+#define rev(x) reverse(all(x))
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout(v) for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+void solve(){
+    ll n,i;
+	ll arr[50] = {0};
+	arr[1] = arr[2] = 2;
+	for(i = 3; i <= 45; i ++) arr[i] = arr[i-1] + arr[i-2];
+	cin>>n;
+	cout<<arr[n]<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    //cin>>t;
+    while(t--) solve();
+    return 0;
+}
