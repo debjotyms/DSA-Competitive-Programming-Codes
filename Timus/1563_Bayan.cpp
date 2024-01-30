91fdc62c08639a33fc5cd6d3197a0a1436cc4773
Debjoty Mitra <debjotyms@gmail.com>
2024-01-30 18:35:28 +0600

    1563. Bayan

---
+++ Timus/1563_Bayan.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define vi vector <long long>
+#define rev(x) reverse(all(x))
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout(v) for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+void solve(){
+    ll n; cin>>n;
+    set<string> st;
+    cin.ignore();
+    for(ll i=0;i<n;i++){
+        string s;
+        getline(cin,s);
+        st.insert(s);
+    }
+    cout<<n-st.size()<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    //cin>>t;
+    while(t--) solve();
+    return 0;
+}
