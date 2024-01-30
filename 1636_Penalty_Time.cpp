d3e073f9e1cfcbc32223f7b88d46abe8d7aaad35
Debjoty Mitra <debjotyms@gmail.com>
2024-01-30 18:34:10 +0600

    1636. Penalty Time

---
+++ 1636_Penalty_Time.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define vi vector <long long>
+#define rev(x) reverse(all(x))
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout(v) for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+void solve(){
+    ll ta,tb; cin>>ta>>tb;
+    ll tot=0;
+    for(ll i=0;i<10;i++){
+        ll a; cin>>a;
+        tot+=a;
+    }
+    if(tot*20+ta<=tb) cout<<"No chance."<<endl;
+    else cout<<"Dirty debug :("<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    //cin>>t;
+    while(t--) solve();
+    return 0;
+}
