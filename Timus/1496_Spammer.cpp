96dc3a02b60dba15a5ccc582399f171f344d5e57
Debjoty Mitra <debjotyms@gmail.com>
2024-01-30 08:00:25 +0600

    1496. Spammer

---
+++ Timus/1496_Spammer.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define vi vector <long long>
+#define rev(x) reverse(all(x))
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout(v) for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+void solve(){
+    ll n; cin>>n;
+    map<string,ll> mp;
+    while(n--){
+        string a; cin>>a;
+        mp[a]+=1;
+    }
+    for(auto x:mp) if(x.second>=2) cout<<x.first<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    // cin>>t;
+    while(t--) solve();
+    return 0;
+}
