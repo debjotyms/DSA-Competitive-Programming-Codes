14111dce2816dbecee63099ae52076267d2a02f9
Debjoty Mitra <debjotyms@gmail.com>
2024-02-01 23:09:01 +0600

    D. Balanced Round

--- Codeforces/D/D_Balanced_Round.cpp
+++ Codeforces/D/D_Balanced_Round.cpp
@@ -1,30 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define pb push_back
 #define vi vector <long long>
+#define rev(x) reverse(all(x))
 #define all(x) x.begin(),x.end()
 #define vin for(auto &x:v) cin>>x;
-#define vout for(auto x:v) cout<<x<<" ";
+#define vout(v) for(auto x:v) cout<<x<<" ";
 #define endl '\n'
 typedef long long ll;
 long long cs = 1;
 
 void solve(){
-    ll n, k; cin>>n>>k;
+    ll n,k; cin>>n>>k;
     vi v(n); vin;
+    vi res;
     sort(all(v));
-    
-    ll ans = 1;
-
-    ll cnt = 1;
-    for(ll i=1;i<n;i++){
-        if(v[i]-v[i-1]>k){
-            cnt=1;
+    ll ans =1;
+    ll temp=1;
+    for(ll i=0;i<n-1;i++){
+        if(v[i+1]-v[i]<=k){
+            temp++;
         }else{
-            cnt++;
+            ans=max(ans,temp);
+            temp=1;
         }
-        ans = max(ans, cnt);
     }
-
+    ans=max(ans,temp);
     cout<<n-ans<<endl;
 }
 
