4995ee36b61c394e303ce8c8781e3e4501c4bdd3
Debjoty Mitra <debjotyms@gmail.com>
2024-02-05 23:08:05 +0600

    A. Jugged Swaps

---
+++ A_Jagged_Swaps.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define vi vector <long long>
+#define rev(x) reverse(all(x))
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout(v) for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+void solve(){
+    ll n; cin>>n;
+    vi v(n+1);
+    for(ll i=1;i<=n;i++){
+        ll a; cin>>a;
+        v[i]=a;
+    }
+    if(v[1]==1) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
