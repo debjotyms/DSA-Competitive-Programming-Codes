2262a3f805ae3009f761c0df84f644300073be11
Debjoty Mitra <debjotyms@gmail.com>
2024-02-06 23:27:08 +0600

    A. Make it White

---
+++ Codeforces/A/A_Make_it_White.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define vi vector <long long>
+#define rev(x) reverse(all(x))
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout(v) for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+void solve(){
+    ll n; cin>>n;
+    ll a=0,b=n-1;
+    string s; cin>>s;
+    while(s[a]=='W') a++;
+    while(s[b]=='W') b--;
+    cout<<b-a+1<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
