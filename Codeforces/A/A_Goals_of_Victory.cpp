5d1517475b4e56047566403a7f3c72ac6bb8ad1e
Debjoty Mitra <debjotyms@gmail.com>
2024-02-10 23:19:28 +0600

    A. Goals of Victory

---
+++ Codeforces/A/A_Goals_of_Victory.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define vi vector <long long>
+#define rev(x) reverse(all(x))
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout(v) for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+void solve(){
+    ll n; cin>>n;
+    ll res=0;
+    for(ll i=0;i<n-1;i++){
+        ll a; cin>>a;
+        res+=a;
+    }
+    cout<<res*-1<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
