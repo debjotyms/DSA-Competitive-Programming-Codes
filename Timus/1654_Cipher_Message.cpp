4a19b3ea6fa5ff3f666d16e37eff3db3c187d8de
Debjoty Mitra <debjotyms@gmail.com>
2024-02-11 09:21:20 +0600

    1654. Cipher Message

---
+++ Timus/1654_Cipher_Message.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define vi vector <long long>
+#define rev(x) reverse(all(x))
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout(v) for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+void solve(){
+    string s; cin>>s;
+    string res = "";
+    ll idx=-1;
+    for(auto x:s){
+        if(res[idx]==x){
+            res.pop_back();
+            idx--;
+        }
+        else{
+            res.push_back(x);
+            idx++;
+        }
+    }
+    cout<<res<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    //cin>>t;
+    while(t--) solve();
+    return 0;
+}
