0dd207dbc8097203153806d2fe00642a9a04d198
Debjoty Mitra <debjotyms@gmail.com>
2024-02-14 23:38:05 +0600

    B. Blank Space

---
+++ Codeforces/B/B_Blank_Space.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define vi vector <long long>
+#define rev(x) reverse(all(x))
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout(v) for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+void solve(){
+    ll n; cin>>n;
+    vi v(n); vin;
+    ll res=0;
+    for(ll i=0;i<n;){
+        ll count=0;
+        while(v[i]==0 and i<n){
+            count++;
+            i++;
+        }
+        if(count==0) i++;
+        res=max(res,count);
+    }
+    cout<<res<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
