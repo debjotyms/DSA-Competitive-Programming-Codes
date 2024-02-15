50f67891b7d82a67d230258f24305a00697b733e
Debjoty Mitra <debjotyms@gmail.com>
2024-02-15 23:05:22 +0600

    A. Sasha and the Beautiful Array

---
+++ Codeforces/A/A_Sasha_and_the_Beautiful_Array.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define vi vector <long long>
+#define rev(x) reverse(all(x))
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout(v) for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+void solve(){
+    ll n; cin>>n;
+    vi v(n); vin;
+    sort(all(v));
+    ll res=0;
+    for(ll i=1;i<n;i++) res+=(v[i]-v[i-1]);
+    cout<<res<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
