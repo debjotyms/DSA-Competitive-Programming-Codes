08d385300abe9fcbdfae3fddcff657dab3fed2f6
Debjoty Mitra <debjotyms@gmail.com>
2024-02-19 20:08:02 +0600

    C. Prepend and Append

---
+++ Codeforces/C/C_Prepend_and_Append.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define vi vector <long long>
+#define rev(x) reverse(all(x))
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout(v) for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+void solve(){
+    ll n; cin>>n;
+    string s; cin>>s;
+    ll start=0, end=n-1;
+    while((s[start]=='0' and s[end]=='1') or (s[start]=='1' and s[end]=='0')){
+        start++; end--;
+        if(start>=end) break;
+    }
+    cout<<end-start+1<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
