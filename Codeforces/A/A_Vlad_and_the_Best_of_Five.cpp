0f5122969056f0f01a84b57595e1a146a7fe9e41
Debjoty Mitra <debjotyms@gmail.com>
2024-02-20 22:47:51 +0600

    A. Vlad and the Best of Five

---
+++ Codeforces/A/A_Vlad_and_the_Best_of_Five.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define vi vector <long long>
+#define rev(x) reverse(all(x))
+#define all(x) x.begin(),x.end()
+#define vin for(auto &x:v) cin>>x;
+#define vout(v) for(auto x:v) cout<<x<<" ";
+#define endl '\n'
+typedef long long ll;
+long long cs = 1;
+
+void solve(){
+    ll a=0,b=0; string s; cin>>s;
+    for(auto x:s){
+        if(x=='A') a++;
+        else b++;
+    }
+    if(a>b) cout<<"A"<<endl;
+    else cout<<"B"<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
