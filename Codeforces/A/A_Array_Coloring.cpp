851011854a0aa004f3cf13975c75861f014abfdf
Debjoty Mitra <debjotyms@gmail.com>
2024-02-21 22:43:27 +0600

    A. Array Coloring

--- Codeforces/A/A_Array_Coloring.cpp
+++ Codeforces/A/A_Array_Coloring.cpp
@@ -1,25 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define pb push_back
 #define vi vector <long long>
+#define rev(x) reverse(all(x))
 #define all(x) x.begin(),x.end()
 #define vin for(auto &x:v) cin>>x;
-#define vout for(auto x:v) cout<<x<<" ";
+#define vout(v) for(auto x:v) cout<<x<<" ";
 #define endl '\n'
 typedef long long ll;
 long long cs = 1;
 
 void solve(){
     ll n; cin>>n;
-    ll od=0, ev=0;
-
-    while(n--){
-        ll a; cin>>a;
-        if(a%2==0) ev++;
-        else od++;
+    vi v(n); vin;
+    sort(all(v));
+    ll sum = accumulate(all(v),0);
+    ll temp = 0;
+    for(ll i=0;i<n;i++){
+        temp+=v[i];
+        sum-=v[i];
+        if(temp%2==sum%2){
+            cout<<"YES"<<endl;
+            return;    
+        }
     }
-
-    if(od%2==0) cout<<"YES"<<endl;
-    else cout<<"NO"<<endl;
+    cout<<"NO"<<endl;
 }
 
 int main(){
