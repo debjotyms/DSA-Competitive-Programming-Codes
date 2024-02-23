2835b87423d3eb33f8996024cddd018ebfb5e928
Debjoty Mitra <debjotyms@gmail.com>
2024-02-23 16:58:52 +0600

    A. United We Stand

--- Codeforces/A/A_United_We_Stand.cpp
+++ Codeforces/A/A_United_We_Stand.cpp
@@ -1,28 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define pb push_back
 #define vi vector <long long>
+#define rev(x) reverse(all(x))
 #define all(x) x.begin(),x.end()
 #define vin for(auto &x:v) cin>>x;
-#define vout for(auto x:v) cout<<x<<" ";
+#define vout(v) for(auto x:v) cout<<x<<" ";
 #define endl '\n'
 typedef long long ll;
 long long cs = 1;
 
 void solve(){
     ll n; cin>>n;
-    vi v(n); vin;
-    ll mx = *max_element(all(v));
-    ll cntMax = count(all(v), mx);
-
-    if(cntMax == n) cout<<-1<<endl;
-    else{
-        ll lb = n - cntMax;
-        ll lc = cntMax;
-        sort(all(v));
-        cout<<lb<<" "<<lc<<endl;
-        for(ll i=0;i<lb;i++) cout<<v[i]<<" "; cout<<endl;
-        for(ll i=0;i<lc;i++) cout<<mx<<" "; cout<<endl;
+    vi v(n); vin; sort(all(v));
+    vi vv;
+    vv.push_back(v[n-1]);
+    for(ll i=n-2;i>=0;i--){
+        if(v[i]==vv[0]) vv.push_back(v[i]);
+        else break;
     }
+    if(vv.size()==n){
+        cout<<-1<<endl;
+        return;
+    }
+    cout<<n-vv.size()<<" "<<vv.size()<<endl;
+    for(ll i=0;i<n-vv.size();i++) cout<<v[i]<<" "; cout<<endl;
+    for(auto x:vv) cout<<x<<" ";
+    cout<<endl;
 }
 
 int main(){
